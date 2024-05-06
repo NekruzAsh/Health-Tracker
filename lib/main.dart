d19a1385639704b1578e10a65f69e5cd49a951d1
NekruzAsh <nekruzashrapov16@gmail.com>
2024-05-06 18:57:07 -0400

    updated spacing

--- lib/main.dart
+++ lib/main.dart
@@ -11,26 +11,29 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            shape: Border.all(width: 2, color: Colors.black),
-            foregroundColor: Colors.black,
-            shadowColor: Colors.black,
-            backgroundColor: Colors.amber.shade900,
-            bottomOpacity: BorderSide.strokeAlignOutside,
-            title: Text('Fitness App'),
-          ),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: const [
-              Expanded(
-                flex: 4,
-                child: Icon(Icons.food_bank),
-              ),
-              Icon(Icons.leaderboard),
-              Icon(Icons.person)
-            ],
-          )),
+        appBar: AppBar(
+          shape: Border.all(width: 2, color: Colors.black),
+          foregroundColor: Colors.black,
+          shadowColor: Colors.black,
+          backgroundColor: Colors.amber.shade900,
+          bottomOpacity: BorderSide.strokeAlignOutside,
+          title: Text('Fitness App'),
+        ),
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center, // Updated line
+          children: const [
+            Expanded(
+              flex: 4,
+              child: Icon(Icons.food_bank),
+            ),
+            SizedBox(width: 16),
+            Icon(Icons.leaderboard),
+            SizedBox(width: 16),
+            Icon(Icons.person),
+          ],
+        ),
+      ),
     );
   }
 }
