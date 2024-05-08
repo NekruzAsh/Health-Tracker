fd2b35c653510444a6f052fed0e0f6034570c719
NekruzAsh <nekruzashrapov16@gmail.com>
2024-05-08 00:42:21 -0400

    set up new classes for the different breakfast items.

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(fontFamily: "Roboto"),
-      home: const HomePage(),
+      home: HomePage(),
     );
   }
 }
